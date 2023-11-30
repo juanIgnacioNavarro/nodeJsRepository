03b6f10579a2ac27df17636b197e82c5142f651f
juanIgnacioNavarro <juanii.navarro12@gmail.com>
2023-11-30 20:52:23 -0300

    integración ecommerce

---
+++ integracion/routes/web.messages.router.js
@@ -0,0 +1,11 @@
+import { Router } from 'express'
+
+export const webMessagesRouter = Router()
+
+webMessagesRouter.get('/', (req,res)=>{
+    res.render('chat.handlebars', { title: 'Mensajes'})
+})
+
+webMessagesRouter.get('/messages', (req,res)=>{
+    res.render('messages.handlebars', { title:'Messages'})
+})
